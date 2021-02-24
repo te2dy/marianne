141d8821ef0f99ca35038e8b69320f4f2f288828
te2dy <52241628+te2dy@users.noreply.github.com>
2021-02-24 13:57:14 +0100

    Enhancements to marianne_loop_comments()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -91,13 +91,35 @@ if ( ! function_exists( 'marianne_loop_comments' ) ) {
 	/**
 	 * The link to comments in the loop.
 	 *
+	 * @param string $container Wrap the link to the comments in a 'div'
+	 *                          or 'footer' block… or nothing.
+	 * @param string $class     If a container is set, add class to it.
+	 *
 	 * @return void
 	 */
-	function marianne_loop_comments() {
+	function marianne_loop_comments( $container = '', $class = '' ) {
 		$comments_number = get_comments_number();
 
-		if ( is_int( $comments_number ) && 0 < $comments_number ) {
+		$allowed_containers = array( 'div', 'footer' );
+		$container_open     = '';
+		$container_close    = '';
+
+		if ( $comments_number && 0 < $comments_number ) :
 			?>
+
+			<?php
+			if ( $container && in_array( $container, $allowed_containers ) ) {
+				$container_open = '<' . esc_attr( $container );
+				if ( $class ) {
+					$container_open .= ' class="' . esc_attr( $class ) . '"';
+				}
+				$container_open .= '>';
+
+				$container_close = '</' . esc_attr( $container ) . '>';
+			}
+			?>
+
+			<?php echo $container_open; ?>
 				<a href="<?php echo esc_url( get_comments_link() ); ?>">
 					<?php
 					printf(
@@ -107,8 +129,10 @@ if ( ! function_exists( 'marianne_loop_comments' ) ) {
 					);
 					?>
 				</a>
+			<?php echo $container_open; ?>
+
 			<?php
-		}
+		endif;
 	}
 }
 
