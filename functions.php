6c8a692eb242548796ec2c72f4ff308b7f5f4c39
te2dy <52241628+te2dy@users.noreply.github.com>
2021-04-04 18:25:27 +0200

    Add post formats

--- functions.php
+++ functions.php
@@ -87,6 +87,22 @@ if ( ! function_exists( 'marianne_setup' ) ) {
 				'style',
 			)
 		);
+
+		/*
+		 * Enables support for Post Formats.
+		 *
+		 * @link https://wordpress.org/support/article/post-formats/
+		 *
+		 * @since Marianne 1.4
+		 */
+		 add_theme_support(
+			 'post-formats',
+			 array(
+				 'audio',
+				 'image',
+				 'video',
+			 )
+		 );
 	}
 
 	add_action( 'after_setup_theme', 'marianne_setup' );
@@ -425,3 +441,68 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/marianne-theme-page.php';
 require get_template_directory() . '/inc/classes/class-marianne-customizer-control-slider.php';
 require get_template_directory() . '/inc/classes/class-marianne-customizer-section-about.php';
+
+/* TEST */
+
+/**
+ * Print the first instance of a block in the content, and then break away.
+ *
+ * @since Twenty Twenty-One 1.0
+ *
+ * @param string      $block_name The full block type name, or a partial match.
+ *                                Example: `core/image`, `core-embed/*`.
+ * @param string|null $content    The content to search in. Use null for get_the_content().
+ * @param int         $instances  How many instances of the block will be printed (max). Defaults to 1.
+ *
+ * @return bool Returns true if a block was located & printed, otherwise false.
+ */
+function twenty_twenty_one_print_first_instance_of_block( $block_name, $content = null, $instances = 1 ) {
+	$instances_count = 0;
+	$blocks_content  = '';
+
+	if ( ! $content ) {
+		$content = get_the_content();
+	}
+
+	// Parse blocks in the content.
+	$blocks = parse_blocks( $content );
+
+	// Loop blocks.
+	foreach ( $blocks as $block ) {
+
+		// Sanity check.
+		if ( ! isset( $block['blockName'] ) ) {
+			continue;
+		}
+
+		// Check if this the block matches the $block_name.
+		$is_matching_block = false;
+
+		// If the block ends with *, try to match the first portion.
+		if ( '*' === $block_name[-1] ) {
+			$is_matching_block = 0 === strpos( $block['blockName'], rtrim( $block_name, '*' ) );
+		} else {
+			$is_matching_block = $block_name === $block['blockName'];
+		}
+
+		if ( $is_matching_block ) {
+			// Increment count.
+			$instances_count++;
+
+			// Add the block HTML.
+			$blocks_content .= render_block( $block );
+
+			// Break the loop if the $instances count was reached.
+			if ( $instances_count >= $instances ) {
+				break;
+			}
+		}
+	}
+
+	if ( $blocks_content ) {
+		echo apply_filters( 'the_content', $blocks_content ); // phpcs:ignore WordPress.Security.EscapeOutput
+		return true;
+	}
+
+	return false;
+}
--- template-parts/content-loop.php
+++ template-parts/content-loop.php
@@ -25,6 +25,21 @@
 		<?php marianne_the_categories( 'entry-meta text-secondary' ); ?>
 
 		<h3 class="entry-title loop-title">
+			<?php
+			if ( has_post_format( array( 'audio', 'image', 'video' ) ) ) : ?>
+				<div class="post-format-icon-container">
+					<?php
+					if ( has_post_format( 'audio' ) ) {
+						marianne_svg( '<path d="M3 18v-6a9 9 0 0 1 18 0v6"></path><path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path>' );
+					} elseif ( has_post_format( 'image' ) ) {
+						marianne_svg( '<rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline>' );
+					} elseif ( has_post_format( 'video' ) ) {
+						marianne_svg( '<polygon points="23 7 16 12 23 17 23 7"></polygon><rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect>' );
+					}
+					?>
+				</div>
+			<?php endif; ?>
+
 			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		</h3>
 
@@ -52,6 +67,10 @@
 			<?php the_excerpt(); ?>
 		</a>
 	</section>
+
+	<?php
+	twenty_twenty_one_print_first_instance_of_block( 'core/image', get_the_content() );
+	?>
 
 	<?php marianne_loop_comments( 'entry-footer loop-footer text-secondary' ); ?>
 </article>
